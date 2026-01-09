a963397206278b633b9df90eb88a048c92d05ff5
Pedro Rossato <Rossato.pedrinho@gmail.com>
2026-01-09 11:33:50 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js')
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once('ready', () => {
+    console.log('Bot ligado com sucesso')
+})
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return
+
+    if (message.content.startsWith('!gasto')) {
+        const partes = message.content.split(' ')
+        const valor = partes[1]
+        const descricao = partes[2]
+        const tipo = partes[3]
+
+        message.reply(
+            `Gasto registrado:\n💰 R$${valor}\n📌 ${descricao}\n📂 ${tipo}`
+        )
+    }
+})
+
+client.login(process.env.TOKEN)
+
+
